04311e1e1e270d738d2f1af20a978bd1baf905f5
soyoudv <noname54765@gmail.com>
2026-01-27 23:55:29 +0100

    utilisation d'un fichier de cache pour permettre à tous les programmes en arrière plan ping_ip d'écrire dans le même tableau pour afficher tout d'un coup à la fin

--- nwscan.sh
+++ nwscan.sh
@@ -3,27 +3,44 @@ set -e
 
 base="192.168."
 
-myIp=$(hostname -I | awk '{print $1}')
+myIp=$(ip route get 192.168.0.1 | grep -oP '(?<=src )[\d.]+')
+usedInterface=$(ip route get 192.168.0.1 | grep -oP '(?<=dev )[\w]+')
+
+
+STATE_FILE="$HOME/.cache/nwscan_state"
+mkdir -p "$(dirname "$STATE_FILE")" #ensure state file directory exists
+: > "$STATE_FILE" # reset state file // : because > alone can fail if noclobber is set
+
+declare -A ip_mac_map # tableau associatif pour stocker IP, MAC et Constructeurs
+declare -A ip_vendor_map # tableau associatif pour stocker IP, et Constructeurs de la MAC associée
 
 ping_ip(){
-    # echo "pinging $ip"
-    macOfThisIp=$(arp -n $ip | awk '/..:..:..:..:..:../ {print $3}')
+    macOfThisIp=$(arp -n $ip -i $usedInterface 2>/dev/null | awk '/..:..:..:..:..:../ {print $3}')
     if [ "$ip" == "$myIp" ]; then
-        macOfThisIp="you"
+        macOfThisIp="1" # ma propre MAC
+    else    
+        if [ -z "$macOfThisIp" ]; then
+        macOfThisIp="0" # pas de MAC trouvée
+        fi
     fi
+
     if ping -c 2 -W 0.6 $ip >/dev/null 2>&1; then
-        echo "$ip : $macOfThisIp"
+        echo "write to state: $ip $macOfThisIp"
+        (echo "$ip $macOfThisIp") >> "$STATE_FILE"
     fi
 }
 
-scan(){
+scan_network(){
+
     for i in $(seq 0 1); do
         for j in $(seq 0 255); do
             ip="$base"$i"."$j
             ping_ip & # TRUC DE GENIE OMGGGGG
        done
     done
+
+    wait
+    cat "$STATE_FILE"
 }
 
-scan
-sleep 2
+scan_network
